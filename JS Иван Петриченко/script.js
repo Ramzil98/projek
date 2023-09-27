86add3d3ac56375b18e688c9c0a1ddc2a8bf9aec
Ramzil <r.gilaziev98@mail.ru>
2023-09-27 23:19:49 +0300

    first commit

---
+++ JS Иван Петриченко/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// const obj = {
+//     name: "Ramzil",
+//     age: 24,
+//     isMarried: true
+// };
+
+// console.log(obj.isMarried)
+
+// let arr = ["orange.jpg", "plum.png", "apple.bmp"];
+// console.log(arr[2])
+
+// alert("Hello");
+
+// const apple = confirm("Are you here?");
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt("как ваше имя?", "");
+// answers[1] = prompt("как ваше фамилия?", "");
+// answers[2] = prompt("сколько вам лет?", "");
+// document.write(answers);
+
+// const category = "toys";
+// console.log("https://someurl.com/" + category)
+
+// const name = "Ramzil";
+// alert(`Hello, ${name}`);
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
